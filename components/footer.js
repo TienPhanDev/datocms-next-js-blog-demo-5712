e5614168cc8b799802930a2bb6146f55a847c2f5
tienphandev <tienphan.dev@gmail.com>
2021-06-08 03:39:42 -0400

    fix

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Technical mentoring and dynamic solutions
+            Made with 💖 in Nova
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -19,7 +19,7 @@ export default function Footer() {
               href="https://github.com/datocms/nextjs-demo"
               className="mx-3 font-bold hover:underline"
             >
-              Our story
+              Learn More
             </a>
           </div>
         </div>
